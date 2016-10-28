6e8a0abf1dee0553334a73873155b28cbcb6980a
Curlyjuan <curlyjuanjuan@gmail.com>
2016-10-28 16:00:17 +0800

    Delete "CLI" & update
    python , flask , flaskr

--- flaskr.py
+++ flaskr.py
@@ -10,74 +10,60 @@
     :license: BSD, see LICENSE for more details.
 """
 
-import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, _app_ctx_stack
 
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 # create our little application :)
-
 app = Flask(__name__)
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
-    SECRET_KEY='development key', 
-    USERNAME='admin',
-    PASSWORD='default' 
-))
+app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
 def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    with open('hardwarelist.txt') as fl:
-        for eachline in fl:
-           (model,sn,user,status)=eachline.strip().split(',')
-           db.execute('insert into entries (model,sn,user,status) values (?, ?, ?, ?)',
-                          (model,sn,user,status))
-    fl.close()
-    db.commit()
-    
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
+    """Creates the database."""
+    with app.app_context():
+       db = get_db()
+       with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+       db.commit()
 
 
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        sqlite_db.row_factory = sqlite3.Row
+        top.sqlite_db = sqlite_db
 
+    return top.sqlite_d
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db_connection(exception):
     """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    top = _app_ctx_stack.top
+    if hasattr(top, 'sqlite_db'):
+        top.sqlite_db.close()
 
 
 @app.route('/')
 def show_entries():
     db = get_db()
+     with open('/var/www/html/flaskr/hardwarelist.txt') as fl:
+          for eachline in fl:
+             (model,sn,user,status)=eachline.strip().split(',')
+             db.execute('insert into entries (model,sn,user,status) values (?, ?, ?, ?)',
+                           (model,sn,user,status))
+    fl.close()
     cur = db.execute('select model,sn,status,user from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
@@ -145,3 +131,7 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+if __name__ == '__main__': 
+    init_db()
+    app.run()
