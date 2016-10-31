2cd374acf9870f8abc6a30c9979167c4dd8240a5
Curlyjuan <curlyjuanjuan@gmail.com>
2016-10-31 15:42:13 +0800

    Back to CLI version
    For a trail  in docker

--- flaskr.py
+++ flaskr.py
@@ -10,60 +10,75 @@
     :license: BSD, see LICENSE for more details.
 """
 
+import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, _app_ctx_stack
+     render_template, flash
 
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
 
 # create our little application :)
+
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key', 
+    USERNAME='admin',
+    PASSWORD='default' 
+))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
 def init_db():
-    """Creates the database."""
-    with app.app_context():
-       db = get_db()
-       with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-       db.commit()
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    with open('hardwarelist.txt') as fl:
+        for eachline in fl:
+           (model,sn,user,status)=eachline.strip().split(',')
+           db.execute('insert into entries (model,sn,user,status) values (?, ?, ?, ?)',
+                          (model,sn,user,status))
+    fl.close()
+    db.commit()
+    
+    
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+
 
 
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-    return top.sqlite_d
 
 @app.teardown_appcontext
-def close_db_connection(exception):
+def close_db(error):
     """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 @app.route('/')
 def show_entries():
     db = get_db()
-     with open('/var/www/html/flaskr/hardwarelist.txt') as fl:
-          for eachline in fl:
-             (model,sn,user,status)=eachline.strip().split(',')
-             db.execute('insert into entries (model,sn,user,status) values (?, ?, ?, ?)',
-                           (model,sn,user,status))
-    fl.close()
     cur = db.execute('select model,sn,status,user from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
@@ -75,7 +90,7 @@ def dosearchuser():
     douser=db.execute('select * from entries where user like ?',
                       [request.form['machine_users']])
     users = douser.fetchall()
-    print users
+  #  print users
     return render_template('searchinguser.html', users=users)
 
     
@@ -85,7 +100,7 @@ def dosearchstatus():
     dostatus=db.execute('select * from entries where status like ?',
                       [request.form['machine_status']])
     dsstatus = dostatus.fetchall()
-    print dsstatus
+ #   print dsstatus
     return render_template('searchingstatus.html', dsstatus=dsstatus)
 
 
@@ -131,7 +146,3 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
-if __name__ == '__main__': 
-    init_db()
-    app.run()
